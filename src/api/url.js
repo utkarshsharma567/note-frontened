711f7dae8ed535c4fdb02a92c415e7bbd9b6d799
utkarshsharma567 <us80999@gmail.com>
2025-12-12 18:39:29 +0530

    Update url.js

--- src/api/url.js
+++ src/api/url.js
@@ -1,6 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 const BACKEND_URL = axios.create({
-  baseURL: 'https://note-backhand-1.onrender.com',
+  baseURL: "https://note-backhand-1.onrender.com"
 });
+
 export default BACKEND_URL;
+
